4e841433c0ebb5e4e4de32b18743e43c03e9a2b8
Jebet812 <cynthiajebet08@gmail.com>
2023-03-27 15:45:14 +0300

    update

---
+++ docker-react/src/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const TodoList = () => {
+  return (
+    <div>
+      <h2>My To-Do List</h2>
+      <ul>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
