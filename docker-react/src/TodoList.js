f69ef290bb2dcd10cc0a97c1559a19eff2f0fe01
Jebet812 <cynthiajebet08@gmail.com>
2023-03-27 16:05:20 +0300

    updated

--- docker-react/src/TodoList.js
+++ docker-react/src/TodoList.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoList = () => {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: 'Task 1' },
+    { id: 2, text: 'Task 2' },
+    { id: 3, text: 'Task 3' }
+  ]);
+  const [inputValue, setInputValue] = useState('');
+  
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  
+  const handleAddTask = () => {
+    if (inputValue !== '') {
+      const newTask = {
+        id: Math.random(),
+        text: inputValue
+      };
+      setTasks([...tasks, newTask]);
+      setInputValue('');
+    }
+  };
+  
+  const handleRemoveTask = (taskId) => {
+    const updatedTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+  
   return (
     <div>
-      <h2>My To-Do List</h2>
       <ul>
-        <li>Task 1</li>
-        <li>Task 2</li>
-        <li>Task 3</li>
+        {tasks.map(task => (
+          <li key={task.id}>
+            {task.text}
+            <button onClick={() => handleRemoveTask(task.id)}>Remove</button>
+          </li>
+        ))}
       </ul>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <button onClick={handleAddTask}>Add Task</button>
     </div>
   );
 };
